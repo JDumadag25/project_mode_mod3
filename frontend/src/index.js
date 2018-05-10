f47528866bc51f146295d3834496a00438d0a25f
JDumadag25 <justin.dumadag@gmail.com>
2018-05-10 09:48:35 -0400

    CORS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+let submit = document.getElementById('zipcode')
+submit.addEventListener('click', function(e){
+  console.log(e)
+})
+console.log(submit)
 
 
-
+fetch('http://localhost:3000/api/v1/bars')
+.then(res => res.json())
+.then(json => console.log(json))
 
 })
