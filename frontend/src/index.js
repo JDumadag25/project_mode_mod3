ad736bb2366bfb4a8588e53326560947a38f4a1c
JDumadag25 <justin.dumadag@gmail.com>
2018-05-10 15:17:51 -0400

    changed checks to radio

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-let submit = document.getElementById('zipcode')
-submit.addEventListener('click', function(e){
-  console.log(e)
-})
-console.log(submit)
+  let post_code = document.getElementById('search')
+  let submit_button = document.getElementById('submit_button')
+
+  submit_button.addEventListener('click', (e) => {
+   let zip_code = post_code.value
+   console.log(zip_code)
+
+  })
 
 
-fetch('http://localhost:3000/api/v1/bars')
-.then(res => res.json())
-.then(json => console.log(json))
 
 })
